dce431cb7595a0bb3726aa1caa3bf556701f8d96
Paulo Xavier <pxavierf@gmail.com>
2021-12-15 15:17:48 -0300

    header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react'
-import { Container, Title } from './styles';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import Logo from '../../assets/logo.svg';
+
+import { Container, Header, TotalCars, HeaderContent } from './styles';
 
 
 export function Home() {
   return (
     <Container>
-     <Title>Home</Title>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
+
+     <Header>
+       <HeaderContent>
+        <Logo
+          width={RFValue(118)}
+          height={RFValue(12)}      
+        />
+
+        <TotalCars>
+          Total de ... carros
+        </TotalCars>
+       </HeaderContent>
+     </Header>
+
     </Container>
   );
 };
